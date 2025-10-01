9a53da9723f369aa5f87720421796c90cf99bd04
Jurandir Prestes <jurandir.prestes@gmail.com>
2025-10-01 19:26:38 -0300

    Create footer.php

---
+++ public_html/footer.php
@@ -0,0 +1,24 @@
+<?php
+
+// footer.php
+
+// Rodapé comum para páginas autenticadas
+
+?>
+
+    </div>
+
+    <!--<div class="footer">
+        <div class="footer-content">
+            <p class="footer-text">
+                &copy; <?php echo date('Y'); ?> Jtek Info. Todos os direitos reservados.
+            </p>
+            <p class="footer-subtext">
+                Controle e Gestão de Ordens de Serviços
+            </p>
+        </div>
+    </div> -->
+
+</body>
+
+</html>
