73f89cbb0c415dbadd39cfd62e14ca281a49a5e0
Jurandir Prestes <jurandir.prestes@gmail.com>
2025-10-01 19:25:58 -0300

    Create logout.php

---
+++ public_html/logout.php
@@ -0,0 +1,16 @@
+<?php
+// logout.php
+// Encerra a sessão do usuário
+
+session_start();
+
+// Destrói todas as variáveis de sessão
+$_SESSION = array();
+
+// Destrói a sessão
+session_destroy();
+
+// Redireciona para a página de login
+header("location: index.php");
+exit;
+?>
